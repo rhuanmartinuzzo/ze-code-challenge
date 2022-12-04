c6f1e8e8347b6304c160403e028e41c9a0873180
Rhuan <rhuanmcr@gmail.com>
2022-12-04 19:35:11 -0300

    Init

---
+++ src/main/java/br/com/zecode/ZeCodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.zecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zecode/ZeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
